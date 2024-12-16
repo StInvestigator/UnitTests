65b2d2ba59cd2ed687c0bd6e6f780b3555cc5a40
Vladyslav Murashko <vladislavuh2000@gmail.com>
2024-12-16 20:47:58 +0200

    Some little fixes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,26 +1,21 @@
 package org.example;
 
 import org.example.models.Product;
+import org.example.models.Storage;
 import org.example.utils.FileManager;
 import org.example.utils.ProductCSVParser;
 import org.example.utils.ProductsDivider;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {/*
-        List<Product> products = ProductCSVParser.parseAllFromCSV(FileManager.readLines("order_1.csv"));
-        products.addAll(ProductCSVParser.parseAllFromCSV(FileManager.readLines("order_2.csv")));
-        List<List<Product>> divided = ProductsDivider.divide(products);
-        divided.forEach(x -> {
-            if (!x.isEmpty()) {
-                FileManager.writeLines(x.get(0)
-                        .getStoreName() + "_res.csv", ProductCSVParser
-                        .parseALLToCSVWithoutStoreName(x));
-            }
-        });*/
+    public static void main(String[] args) {
+        Storage storage = new Storage(new ArrayList<Product>(),new ProductCSVParser(), new ProductsDivider());
+        storage.readFromFiles(List.of("order_1.csv","order_2.csv"));
+        storage.divideAndSave();
     }
 }
